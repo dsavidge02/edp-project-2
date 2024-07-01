60565c417f8746f91e0e03e958a5c93bdeafab3b
Dan Savidge <dsavidge02@gmail.com>
2024-07-01 13:23:21 -0400

    updated server.js

--- server/server.js
+++ server/server.js
@@ -31,6 +31,188 @@ app.get("/api/planets", async (req, res) => {
   }
 });
 
+app.get("/api/films", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collFilms);
+    const films = await collection.find({}).toArray();
+    res.json(films);
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("Death star blew the film up - no film found");
+  }
+});
+
+app.get("/api/characters", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collChars);
+    const characters = await collection.find({}).toArray();
+    res.json(characters);
+  } catch (err) {
+    console.error("Error:", err);
+    res
+      .status(500)
+      .send("Death star blew the character up - no character found");
+  }
+});
+
+app.get("/api/planets/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collPlans);
+    const result = await collection.findOne({ id: id });
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send("No planet found");
+    }
+  } catch (err) {
+    console.error("Error", err);
+    res.status(500).send("Death star blew the planet up - no planet found");
+  }
+});
+
+app.get("/api/characters/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collChars);
+    const result = await collection.findOne({ id: id });
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send("No character found");
+    }
+  } catch (err) {
+    console.error("Error", err);
+    res
+      .status(500)
+      .send("Death star blew the character up - no character found");
+  }
+});
+
+app.get("/api/films/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collFilms);
+    const result = await collection.findOne({ id: id });
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send("No film found");
+    }
+  } catch (err) {
+    console.error("Error", err);
+    res.status(500).send("Death star blew the film up - no film found");
+  }
+});
+
+app.get("/api/films/:id/characters", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collFilmsChars);
+    const result = await collection.find({ film_id: id }).toArray();
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send("No characters found");
+    }
+  } catch (err) {
+    console.error("Error", err);
+    res
+      .status(500)
+      .send("Death star blew the characters up - no characters found");
+  }
+});
+
+app.get("/api/films/:id/planets", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collFilmsPlans);
+    const result = await collection.find({ film_id: id }).toArray();
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send("No planets found");
+    }
+  } catch (err) {
+    console.error("Error", err);
+    res.status(500).send("Death star blew the planets up - no planets found");
+  }
+});
+
+app.get("/api/characters/:id/films", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collFilmsChars);
+    const result = await collection.find({ character_id: id }).toArray();
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send("No films found");
+    }
+  } catch (err) {
+    console.error("Error", err);
+    res.status(500).send("Death star blew the films up - no films found");
+  }
+});
+
+app.get("/api/planets/:id/films", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collFilmsPlans);
+    const result = await collection.find({ planet_id: id }).toArray();
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send("No films found");
+    }
+  } catch (err) {
+    console.error("Error", err);
+    res.status(500).send("Death star blew the films up - no films found");
+  }
+});
+
+app.get("/api/planets/:id/characters", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collChars);
+    const result = await collection
+      .find({ homeworld: id })
+      .project({ _id: 0, id: 1, name: 1 })
+      .toArray();
+
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send("No characters found");
+    }
+  } catch (err) {
+    console.error("Error", err);
+    res
+      .status(500)
+      .send("Death star blew the characters up - no characters found");
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
